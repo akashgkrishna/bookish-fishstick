b67ac2c221bc66611cc6e5875bb41ba53b1601ef
Akash Krishna <krishna@Akashs-MacBook-Air.local>
2025-05-25 15:35:00 +0530

    Added example for has-a relationship

---
+++ src/has/a/relationship/computer/example/Memory.java
@@ -0,0 +1,11 @@
+package has.a.relationship.computer.example;
+
+/**
+ * Component class representing Memory
+ */
+
+public class Memory {
+    void loadMemory() {
+        System.out.println("Loading up memory");
+    }
+}
